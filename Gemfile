854c7cc4b42ea67ab07cf45f4e3499ee8dfed0ec
SANO Hiroki <sanoche16@gmail.com>
2016-12-11 19:05:35 +0900

    bind puma with unix socket

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'capistrano-rails'
   gem 'capistrano-rvm'
   gem 'capistrano-bundler'
+  gem 'capistrano-puma'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,12 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# dir
+app_dir = File.expand_path("../..", __FILE__)
+
+# sock
+bind "unix:///#{app_dir}/tmp/sockets/puma.sock"
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
